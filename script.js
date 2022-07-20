1b9762f3547a749c8f8f05306b5ba4120cc17505
tommy <tcatty7@gmail.com>
2022-07-20 14:06:55 -0400

    x

--- script.js
+++ script.js
@@ -46,3 +46,24 @@ function cat() {
  window.location.reload()
 }
 
+function customAlert() {
+        this.render = function (dialog) {
+            var winW = window.innerWidth;
+            var winH = window.innerHeight;
+            var dialogoverlay = document.getElementById('dialogoverlay');
+            var dialogbox = document.getElementById('dialogbox');
+            dialogoverlay.style.display = "block";
+            dialogoverlay.style.height = winH + "px";
+            dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+            dialogbox.style.top = "100px";
+            dialogbox.style.display = "block"
+            document.getElementById('dialogboxhead').innerHTML = '<h1 align="center">hello there</h1>'
+            document.getElementById('dialogboxbody').innerHTML = dialog;
+            document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">click me</button>';
+        }
+        this.ok = function () {
+            document.getElementById('dialogoverlay').style.display = 'none'   
+            document.getElementById('dialogbox').style.display = 'none'
+        };
+    }
+var Alert = new customAlert();
