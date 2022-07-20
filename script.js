e0bdf84c6bd8714da889b213d261bdfe622e7bf5
tommy <tcatty7@gmail.com>
2022-07-20 13:22:24 -0400

    cx

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let display = document.getElementById('display');
+let display = document.getElementById('display');  
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
@@ -19,12 +19,30 @@ switch(e.target.innerText){
         try{
             display.innerText = eval(display.innerText);
         } catch{
+            let b =
             display.innerText = 'invaled expresion' 
         }
-
         break;
     default:display.innerText += e.target.innerText
+    }
+});
+});
+function cat() {
+    let red;
+    if(confirm('do you want to change the font to dark theme') == true) {
+        document.getElementById('equal').style.backgroundColor = 'darkblue';
+        document.getElementById('equal1').style.backgroundColor = 'darkorange'; 
+        document.getElementById('arrow').style.backgroundColor = 'purple'; 
+        document.getElementById('cool').style.backgroundColor = 'black';
+        document.getElementById('cool').style.color = 'white';
+     let a =   document.getElementById('display');
+     a.style.color = 'black'; a.style.backgroundColor = 'darkgrey';
+     document.querySelector('.buttons').style.backgroundColor = 'black';
+     document.querySelector('.buttons').style.color = 'white';
+    }else {
+        event.preventDefault
+    } }
+    function fub() {
+ window.location.reload()
 }
 
-});
-});
